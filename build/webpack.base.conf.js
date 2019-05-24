e10c45db2455f6b787e559b10b6044b393bc3675
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2019-05-25 00:38:07 +0300

    add pug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,12 +3,33 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const fs = require('fs')
 
 const PATHS = {
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
   assets: 'assets/'
 }
+// Our function that generates our html plugins
+function generateHtmlPlugins (templateDir) {
+  // Read files in template directory
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.')
+    const name = parts[0]
+    const extension = parts[1]
+    // Create new HTMLWebpackPlugin with options
+    return new HtmlWebpackPlugin({
+      hash: false,
+      template: path.resolve(__dirname, `${templateDir}/${name}.pug`),
+      filename: `${name}.html`
+    })
+  })
+}
+
+// Call our function on our views directory.
+const htmlPlugins = generateHtmlPlugins(`${PATHS.src}/template/pages`)
 
 module.exports = {
   // BASE config
@@ -21,7 +42,7 @@ module.exports = {
   output: {
     filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
-    publicPath: '/'
+    publicPath: ''
   },
   module: {
     rules: [{
@@ -71,6 +92,9 @@ module.exports = {
           options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
         }
       ]
+    }, {
+      test: /\.pug$/,
+      use: ['pug-loader']
     }]
   },
   resolve: {
@@ -83,15 +107,15 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].css`,
     }),
-    // Copy HtmlWebpackPlugin and change index.html for another html page
-    new HtmlWebpackPlugin({
-      hash: false,
-      template: `${PATHS.src}/index.html`,
-      filename: './index.html'
-    }),
+    // new HtmlWebpackPlugin({
+    //   hash: false,
+    //   template: `${PATHS.src}/pages/index.pug`,
+    //   filename: `./index.html`
+    // }),
     new CopyWebpackPlugin([
       { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
       { from: `${PATHS.src}/static`, to: '' },
     ])
-  ],
+  ]
+    .concat(htmlPlugins)
 }
--- src/index.js
+++ src/index.js
@@ -5,17 +5,27 @@ import './scss/main.scss'
 // CSS (example)
 import './css/main.css'
 
-// Bootstrap grid
-// import BootstrapGrid from 'bootstrap/dist/css/bootstrap-grid.css'
-
-
+// import 'vue'
+// import Vue from 'vue'
+// import store from './store'
 window.Vue = require('vue')
 
 // Components
-Vue.component('the-header', require('./components/TheHeader.vue').default)
-Vue.component('the-footer', require('./components/TheFooter.vue').default)
+Vue.component('Carousel', require('./components/vue-carousel/Carousel.vue').default)
+Vue.component('Slide', require('./components/vue-carousel/Slide.vue').default)
+Vue.component('SweetModal', require('./components/sweet-modal-vue/components/SweetModal.vue').default)
 
 // Plugins
+// element-ui
+import lang from 'element-ui/lib/locale/lang/ru-RU'
+import locale from 'element-ui/lib/locale'
+locale.use(lang)
+import Col from 'element-ui/packages/col/index.js';
+import Row from 'element-ui/packages/row/index.js';
+Vue.use(Row)
+Vue.use(Col)
+import 'element-ui/lib/theme-chalk/row.css'
+import 'element-ui/lib/theme-chalk/col.css'
 
 const app = new Vue({
   el: '#app'
